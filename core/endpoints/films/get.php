cd2d55d511f191997f49d1342779c7e1d86638d0
Caleb Ely <le717@users.noreply.github.com>
2020-06-30 19:26:05 -0400

    Add endpoint code for basic film GET

--- core/endpoints/films/get.php
+++ core/endpoints/films/get.php
@@ -1,3 +1,18 @@
 <?php
   require_once '../core/common-utils.php';
-  echo make_response(204);
+  require_once '../core/classes/Film.php';
+
+  // A film id was not provided
+  if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    echo make_error_response(400, 'A Film ID must be provided!');
+  }
+
+  // Get the film
+  $film = new Film(escape_xss($_GET['id']));
+
+  // That film doesn't exist
+  if ($film->exists() === false) {
+    echo make_error_response(404, "Could not find Film for ID {$film->id}!");
+  }
+
+  echo make_response(200, $film->info());
