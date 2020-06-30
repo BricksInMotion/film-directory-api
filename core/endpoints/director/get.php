bf00b086d807e11f5d5246668be86fc6b20f6039
Caleb Ely <le717@users.noreply.github.com>
2020-06-30 17:59:40 -0400

    Missing the GET response

--- core/endpoints/director/get.php
+++ core/endpoints/director/get.php
@@ -1,3 +1,20 @@
 <?php
-  require_once '../core/common-utils.php';
-  echo make_response(204);
+require_once '../core/common-utils.php';
+require_once '../core/classes/Director.php';
+
+// A director id was not provided
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+  echo make_error_response(400, 'Missing Director user ID!');
+}
+
+// Get the director ID
+$director = new Director(escape_xss($_GET['id']));
+// $search_results = Search::by_title($search_query);
+
+// No results were found
+if ($director->exists() === false) {
+  echo make_error_response(404, "Could not find Director for ID {$director->id}!");
+}
+
+// We have a Director, get their info
+echo make_response(200, $director->info());
