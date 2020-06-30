9e3b169011584d6149cabdc15bc9195f94b93846
Caleb Ely <le717@users.noreply.github.com>
2020-06-30 19:23:41 -0400

    Return director ID in film info
    That way, a request to director can be performed to get that info

--- core/sql/film-info.sql
+++ core/sql/film-info.sql
@@ -1,9 +1,11 @@
 SELECT
+  `id`,
   `title`,
   `user_desc` AS `desc`,
   `img_thumb` AS `thumbnail`,
   `lenth` AS `length`,
-  `date_create` AS `release_date`
+  `date_create` AS `release_date`,
+  `user_id` AS `director_id`
 FROM `films`
 WHERE `films`.`id`= ?
 LIMIT 1;
