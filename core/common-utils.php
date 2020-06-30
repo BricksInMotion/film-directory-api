352a2d38344caf3efb1cbef31b37be60408fe31d
Caleb Ely <le717@users.noreply.github.com>
2020-06-30 16:11:10 -0400

    If we have an endpoint response, execute it

--- core/common-utils.php
+++ core/common-utils.php
@@ -16,24 +16,31 @@ function escape_xss($text) {
 }
 
 
+/**
+ * Register all responses for this this endpoint.
+ * @param {string} $verb - The HTTP verb used in the request.
+ * @param {string} $endpoint - The endpoint name being accessed.
+ */
 function register_endpoint($verb, $endpoint) {
   // Build out a path to the endpoint's responses
   $verb_lower = strtolower($verb);
   $require_path = $_SERVER['DOCUMENT_ROOT'] . "/core/endpoints/{$endpoint}";
-  $action_file = "{$require_path}/{$verb_lower}.php";
+  $response_file = "{$require_path}/{$verb_lower}.php";
 
   // A response for that verb is not available
-  if (!file_exists($action_file)){
+  if (!file_exists($response_file)){
     make_error_response(405, "Unsupported verb {$verb}.", [generate_allow_header($require_path)]);
   }
 
-  // We have a response, ??
-  make_response(200, ['all good']);
+  // We have a response, execute it
+  require_once $response_file;
 }
 
 
 /**
  * Generate an Allow header for an endpoint.
+ * @param {string} $endpoint_path - The folder path to the endpoint's responses.
+ * @return {string} A fully-formed HTTP Allow header.
  */
 function generate_allow_header($endpoint_path) {
   // Dynamically determine what actions are available
