e77098643d34aaa7fff89f9a94f486e2f67b7e87
Caleb Ely <le717@users.noreply.github.com>
2020-06-30 19:09:45 -0400

    Correctly indent files

--- core/endpoints/director/get.php
+++ core/endpoints/director/get.php
@@ -1,68 +1,68 @@
 <?php
-require_once '../core/common-utils.php';
-require_once '../core/classes/Director.php';
+  require_once '../core/common-utils.php';
+  require_once '../core/classes/Director.php';
 
-// A director id was not provided
-if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
-  echo make_error_response(400, 'Missing Director user ID!');
-}
-
-// Get the director ID
-$director = new Director(escape_xss($_GET['id']));
-
-// No results were found
-if ($director->exists() === false) {
-  echo make_error_response(404, "Could not find Director for ID {$director->id}!");
-}
-
-
-// We have a Director and not requesting filmography, send back just their info
-if (!isset($_GET['roles'])) {
-  echo make_response(200, $director->info());
-}
-
-// We want to get the filmography
-if (isset($_GET['roles'])) {
-  $roles = escape_xss($_GET['roles']);
-
-  // If the roles key is given, at least one role must be requested
-  if (empty($roles)) {
-    echo make_error_response(400, 'At least one Director role must be provided!');
+  // A director id was not provided
+  if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    echo make_error_response(400, 'Missing Director user ID!');
   }
 
-  // Convert it into a list
-  $roles = explode(',', $roles);
+  // Get the director ID
+  $director = new Director(escape_xss($_GET['id']));
 
-  // Map the role labels to the fetch methods
-  $filmography = [];
-  $role_methods = [
-    'director' => 'as_director',
-    'writer'   => 'as_writer',
-    'composer' => 'as_composer',
-    'animator' => 'as_animator',
-    'editor'   => 'as_editor',
-    'vfx'      => 'as_vfx',
-    'sound'    => 'as_sound',
-    'other'    => 'as_crew',
-    'thanks'   => 'as_thanks',
-    'va'       => 'as_voice',
-  ];
+  // No results were found
+  if ($director->exists() === false) {
+    echo make_error_response(404, "Could not find Director for ID {$director->id}!");
+  }
 
-  // If a special "all" key is given, collect all the info
-  if (count($roles) === 1 && $roles[0] === "all") {
-    foreach ($role_methods as $label => $func) {
-      $filmography[$label] = $director->$func();
+
+  // We have a Director and not requesting filmography, send back just their info
+  if (!isset($_GET['roles'])) {
+    echo make_response(200, $director->info());
+  }
+
+  // We want to get the filmography
+  if (isset($_GET['roles'])) {
+    $roles = escape_xss($_GET['roles']);
+
+    // If the roles key is given, at least one role must be requested
+    if (empty($roles)) {
+      echo make_error_response(400, 'At least one Director role must be provided!');
+    }
+
+    // Convert it into a list
+    $roles = explode(',', $roles);
+
+    // Map the role labels to the fetch methods
+    $filmography = [];
+    $role_methods = [
+      'director' => 'as_director',
+      'writer'   => 'as_writer',
+      'composer' => 'as_composer',
+      'animator' => 'as_animator',
+      'editor'   => 'as_editor',
+      'vfx'      => 'as_vfx',
+      'sound'    => 'as_sound',
+      'other'    => 'as_crew',
+      'thanks'   => 'as_thanks',
+      'va'       => 'as_voice',
+    ];
+
+    // If a special "all" key is given, collect all the info
+    if (count($roles) === 1 && $roles[0] === "all") {
+      foreach ($role_methods as $label => $func) {
+        $filmography[$label] = $director->$func();
+      }
+      echo make_response(200, $filmography);
+    }
+
+    // For each given key, get the info
+    foreach ($roles as $label) {
+      // ...But ignoring invalid keys
+      if (array_key_exists($label, $role_methods)) {
+        $func = $role_methods[$label];
+        $filmography[$label] = $director->$func();
+      }
     }
     echo make_response(200, $filmography);
   }
-
-  // For each given key, get the info
-  foreach ($roles as $label) {
-    // ...But ignoring invalid keys
-    if (array_key_exists($label, $role_methods)) {
-      $func = $role_methods[$label];
-      $filmography[$label] = $director->$func();
-    }
-  }
-  echo make_response(200, $filmography);
-}
--- core/endpoints/search/get.php
+++ core/endpoints/search/get.php
@@ -1,20 +1,20 @@
 <?php
-require_once '../core/common-utils.php';
-require_once '../core/classes/Search.php';
+  require_once '../core/common-utils.php';
+  require_once '../core/classes/Search.php';
 
-// A search value was not provided
-if (!isset($_GET['title'])) {
-  echo make_error_response(400, 'Missing search value!');
-}
+  // A search value was not provided
+  if (!isset($_GET['title'])) {
+    echo make_error_response(400, 'Missing search value!');
+  }
 
-// Get the search value
-$search_query = escape_xss($_GET['title']);
-$search_results = Search::by_title($search_query);
+  // Get the search value
+  $search_query = escape_xss($_GET['title']);
+  $search_results = Search::by_title($search_query);
 
-// No results were found
-if (empty($search_results)) {
-  echo make_error_response(404, "No films found for query {$search_query}");
-}
+  // No results were found
+  if (empty($search_results)) {
+    echo make_error_response(404, "No films found for query {$search_query}");
+  }
 
-// We have results
-echo make_response(200, $search_results);
+  // We have results
+  echo make_response(200, $search_results);
