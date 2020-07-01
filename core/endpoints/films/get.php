0b86790ca560678f0a1597488c019e33a5be3d4d
Caleb Ely <le717@users.noreply.github.com>
2020-07-01 12:52:05 -0400

    Impl film GET response

--- core/endpoints/films/get.php
+++ core/endpoints/films/get.php
@@ -15,4 +15,53 @@
     echo make_error_response(404, "Could not find Film for ID {$film->id}!");
   }
 
-  echo make_response(200, $film->info());
+  // We have a film and not requesting additional info, just fetch the basic info
+  if (!isset($_GET['props'])) {
+    echo make_response(200, $film->info());
+  }
+
+  // Map the property values to the fetch methods
+  $prop_methods = [
+    'links'      => 'links',
+    'genres'     => 'genres',
+    'rating'     => 'rating',
+    'reviews'    => 'reviews',
+    'cast_crew'  => 'cast_crew',
+    'advisories' => 'advisories',
+    'staff_ratings' => 'staff_ratings',
+  ];
+
+  // We want to get film properties
+  $props = escape_xss($_GET['props']);
+
+  // If the roles key is given, at least one role must be requested
+  if (empty($props)) {
+    $possible_values = array_keys($prop_methods);
+    sort($possible_values, SORT_STRING);
+    $possible_values = implode(', ', $possible_values);
+    echo make_error_response(400, "At least one Film property must be provided! Possible values: {$possible_values}");
+  }
+
+  // Convert the requested roles into a list
+  $props = explode(',', $props);
+  $film_properties = [];
+
+  // If a special "all" key is given, collect all the info
+  if (count($props) === 1 && $props[0] === "all") {
+    foreach ($prop_methods as $label => $func) {
+      $film_properties['info'] = $film->info();
+      $film_properties[$label] = $film->$func();
+    }
+    echo make_response(200, $film_properties);
+  }
+
+  // For each given key, get the info
+  foreach ($props as $label) {
+    // ...But ignoring invalid keys
+    if (array_key_exists($label, $prop_methods)) {
+      $func = $prop_methods[$label];
+      $film_properties[$label] = $film->$func();
+    }
+  }
+  echo make_response(200, $film_properties);
+
