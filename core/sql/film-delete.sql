64303be0d745bf443b959a27f58a70d4728937f6
Caleb Ely <le717@users.noreply.github.com>
2020-07-01 16:33:06 -0400

    Fill in film deletion queries

--- core/sql/film-delete.sql
+++ core/sql/film-delete.sql
@@ -1,2 +1,12 @@
+DELETE FROM films_user_rate WHERE id = CONCAT('film', :id);
+DELETE FROM films_user_rate id REGEXP CONCAT('^rev..', :id, '$');
+
+DELETE FROM films_user_rate_votes WHERE id = CONCAT('film', :id);
+DELETE FROM films_user_rate_votes id REGEXP CONCAT('^rev..', :id, '$');
+
+DELETE FROM films_reviews WHERE filmid = :id;
+DELETE FROM films_links WHERE film_id = :id;
+DELETE FROM films_castcrew WHERE film_id = :id;
+DELETE FROM films_genre WHERE film_id = :id;
+
 DELETE FROM films WHERE id = :id;
--- TODO Delete all other info too bc I don't think this will cascade
--- core/sql/film-rating.sql
+++ core/sql/film-rating.sql
@@ -3,4 +3,4 @@ SELECT
   ROUND(total_value / total_votes, 2) AS raw_rating,
   (SELECT IFNULL(raw_rating, FORMAT(0, 0))) AS rating
 FROM films_user_rate
-WHERE films_user_rate.id = CONCAT("film", :id);
+WHERE films_user_rate.id = CONCAT('film', :id);
