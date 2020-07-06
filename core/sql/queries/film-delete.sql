488e3d3751f1db455fd941d1d9dd5fa4074fa87f
Caleb Ely <le717@users.noreply.github.com>
2020-07-06 16:57:44 -0400

    Fix film deletion

--- core/sql/queries/film-delete.sql
+++ core/sql/queries/film-delete.sql
@@ -1,10 +1,8 @@
--- Ideally there would be FK constains that would cascade down on deletion,
--- as well as the *user_rate* tables not use this weird PK format
 DELETE FROM films_user_rate WHERE id = CONCAT('film', :id);
-DELETE FROM films_user_rate id REGEXP CONCAT('^rev..', :id, '$');
+DELETE FROM films_user_rate WHERE id REGEXP CONCAT('^rev..', :id, '$');
 
-DELETE FROM films_user_rate_votes WHERE id = CONCAT('film', :id);
-DELETE FROM films_user_rate_votes id REGEXP CONCAT('^rev..', :id, '$');
+DELETE FROM films_user_rate_votes WHERE rating_id = CONCAT('film', :id);
+DELETE FROM films_user_rate_votes WHERE rating_id REGEXP CONCAT('^rev..', :id, '$');
 
 DELETE FROM films_reviews WHERE filmid = :id;
 DELETE FROM films_links WHERE film_id = :id;
