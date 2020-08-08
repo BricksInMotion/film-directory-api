9cebafbccba4bed392239183be2c69dd36375820
Caleb Ely <le717@users.noreply.github.com>
2020-08-08 17:20:10 -0400

    Expose film thumbnail in feed

--- core/sql/queries/feed-latest.sql
+++ core/sql/queries/feed-latest.sql
@@ -1,7 +1,8 @@
 SELECT films.id,
   films.title,
   films.user_id,
-  forums_users.username AS `user_name`
+  forums_users.username AS `user_name`,
+  films.img_thumb AS thumbnail
 FROM films
 JOIN forums_users ON forums_users.id = films.user_id
 ORDER BY films.id DESC LIMIT 8;
