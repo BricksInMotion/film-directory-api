e34a06fe9fb34cf3717ef0ef2e9cfdb061f9fc4b
Caleb Ely <le717@users.noreply.github.com>
2020-12-28 14:33:18 -0500

    Return thumbnail file name too

--- core/sql/queries/archive-get-films-in-year.sql
+++ core/sql/queries/archive-get-films-in-year.sql
@@ -1,4 +1,8 @@
-SELECT id, title, date_create AS `date`
+SELECT
+  id,
+  title,
+  date_create AS `date`,
+  img_thumb AS thumbnail
 FROM films
 WHERE YEAR(date_create) = :year
 ORDER BY YEAR(date_create) ASC;
